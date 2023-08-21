85e4cccaefeee7b8f56db7125adec154d7c8ee6c
Davi Estrella <daviestrella66@gmail.com>
2023-08-20 21:12:53 -0300

    Corrige bugs e adiciona efeitos

--- script.js
+++ script.js
@@ -2,30 +2,43 @@ let altura = document.getElementById("altura");
 let peso = document.getElementById("peso"); 
 let descricao = document.getElementById("descricao");
 const definicao = "Seu IMC é classificado como: ";
-
+const brake = undefined; 
 document.getElementById("calcBtn").onclick = function(){
-    let resultado = peso.value/ (altura.value * altura.value); 
-    if(resultado <= 18.5){
-        descricao.innerHTML = definicao + "Magreza " + resultado.toFixed(2);
-    }
-    else if(resultado >= 18.5 && resultado <= 24.9){
-        descricao.innerHTML = definicao + "Peso normal " + resultado.toFixed(2);
+let resultado = peso.value/ (altura.value * altura.value); 
+
+    if(peso.value <= 0 || altura.value <=0){
+        document.getElementById("alertBox").style.visibility = "visible";
+        console.log(peso, altura)
+        descricao.innerHTML = "Não foi possível definir seu IMC."
+        resultado = brake; 
+    }else{
+        document.getElementById("alertBox").style.visibility = "hidden";
     }
 
-    else if(resultado >= 25 && resultado <= 29.9){
+    if(resultado != brake){
+        if(resultado <= 18.5){
+            descricao.innerHTML = definicao + "Magreza " + resultado.toFixed(2);
+        }
+        else if(resultado >= 18.5 && resultado <= 24.9){
+            descricao.innerHTML = definicao + "Peso normal " + resultado.toFixed(2);
+        }
+
+        else if(resultado >= 25 && resultado <= 29.9){
         descricao.innerHTML = definicao + "Sobrepeso "+ resultado.toFixed(2);
-    }
+        }
 
-    else if(resultado >= 30 && resultado <= 34.9){
+        else if(resultado >= 30 && resultado <= 34.9){
         descricao.innerHTML = definicao + "Obesidade grau I "+ resultado.toFixed(2);
-    }
+        }
 
-    else if(resultado >= 35 && resultado <= 40){
+        else if(resultado >= 35 && resultado <= 40){
         descricao.innerHTML = definicao + "Obesidade grau II "+ resultado.toFixed(2);
-    }
+        }
 
-    else if(resultado > 40){
+        else if(resultado > 40){
         descricao.innerHTML = definicao + "Obesidade grau III "+ resultado.toFixed(2);
     }
+    }
+    
     
 }
